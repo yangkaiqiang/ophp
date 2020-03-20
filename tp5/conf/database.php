5b0cfae5a29e290af95d6db4a9d53e5abcbf8e28
yangkaiqiang <513092734@qq.com>
2020-03-20 15:09:33 +0800

    commit1

---
+++ tp5/conf/database.php
@@ -0,0 +1,13 @@
+<?php
+return [
+    // 数据库名
+    'database'        => 'tp_shop',
+    // 用户名
+    'username'        => 'root',
+    // 密码
+    'password'        => 'root',
+    // 端口
+    'hostport'        => '3306',
+    // 数据库表前缀
+    'prefix'          => 'tp_',
+];
